c18700399cbbe2ad477b43467c022f07f19980e0
Pedro Henrique Martins Souza <pedrocatins29@hotmail.com>
2020-05-25 17:36:59 -0300

    sera que eu tenho saude mental?

--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
-// import express from "express";
-// import { ApolloServer } from "apollo-server-express";
-// import { gql } from "apollo-server";
-// import db from "./config/connection";
-const express = require("express");
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { gql } from "apollo-server";
+import db from "./config/connection";
 const app = express();
 
-// const typeDefs = gql`
-//   type Query {
-//     hello: String
-//   }
-// `;
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-// const resolvers = {
-//   Query: {
-//     hello: () =>
-//       new Promise((resolve, reject) => {
-//         db.query("select * from user", (error, result) => {
-//           if (error) {
-//             reject(error);
-//           } else {
-//             resolve(JSON.stringify(result));
-//           }
-//         });
-//       }),
-//   },
-// };
+const resolvers = {
+  Query: {
+    hello: () =>
+      new Promise((resolve, reject) => {
+        db.query("select * from user", (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.stringify(result));
+          }
+        });
+      }),
+  },
+};
 
 const port = process.env.PORT || 3000;
 
-// const apolloServer = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-// });
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-// apolloServer.applyMiddleware({ app });
+apolloServer.applyMiddleware({ app });
 
 app.listen(port, () => {
   console.log("API RODANDO COM SUCESSO");
