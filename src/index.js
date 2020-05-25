2e2ece8e51cf1049c9c417194b45487f5de020ce
Pedro Henrique Martins Souza <pedrocatins29@hotmail.com>
2020-05-25 16:09:49 -0300

    Subindo

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ const apolloServer = new ApolloServer({
 apolloServer.applyMiddleware({ app });
 
 app.get("/", (req, res) => {
-  res.send("Hello Skillus com CICD???");
+  res.send("Hello Skillus ajisbdoiasd");
 });
 
 app.listen(port, () => {
