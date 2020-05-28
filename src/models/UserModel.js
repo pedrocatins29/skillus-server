276361e0683e36052281acf72f814ff44f00dfa4
Pedro Henrique Martins Souza <pedrocatins29@hotmail.com>
2020-05-28 11:50:20 -0300

    Subindo um order By maroto no retorno de skills e de media de usuario

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,80 +1,76 @@
 import db from "../config/database";
 
 export const userModel = {
-    all() {
-        return new Promise((resolve, reject) => {
-            const query = `SELECT U.*, US.name AS status
+  all() {
+    return new Promise((resolve, reject) => {
+      const query = `SELECT U.*, US.name AS status
             FROM user U
-            INNER JOIN user_status US ON US.id = U.user_status_id;`;
-            db.query(query, (error, result) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(result);
-                }
-            });
-        });
-    },
+            INNER JOIN user_status US ON US.id = U.user_status_id order by media DESC;`;
+      db.query(query, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  },
 
-    get(id) {
-        return new Promise((resolve, reject) => {
-            db.query(
-                `SELECT * FROM user WHERE id = ${id}`,
-                (error, result) => {
-                    if (error) {
-                        reject(error);
-                    } else {
-                        resolve(result[0]);
-                    }
-                }
-            );
-        });
-    },
+  get(id) {
+    return new Promise((resolve, reject) => {
+      db.query(`SELECT * FROM user WHERE id = ${id}`, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result[0]);
+        }
+      });
+    });
+  },
 
-    getUserSkills(userId) {
-        return new Promise((resolve, reject) => {
-            const query = `SELECT US.id, US.skill_id, S.name, US.rating FROM user_skill US
+  getUserSkills(userId) {
+    return new Promise((resolve, reject) => {
+      const query = `SELECT US.id, US.skill_id, S.name, US.rating FROM user_skill US
             INNER JOIN skill S on S.id = US.skill_id 
-            WHERE US.user_id = ${userId}`;
-            db.query(query, (error, result) => {
-                    if (error) {
-                        reject(error);
-                    } else {
-                        resolve(result);
-                    }
-                }
-            );
-        });
-    },
+            WHERE US.user_id = ${userId} order by rating DESC`;
+      db.query(query, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  },
 
-    getUserRating(userId) {
-        return new Promise((resolve, reject) => {
-            const query = `CALL MEDIA(${userId})`;
-            
-            db.query(query, (error, result) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(result[0][0].media);
-                }
-            });
-        });
-    },
+  getUserRating(userId) {
+    return new Promise((resolve, reject) => {
+      const query = `CALL MEDIA(${userId})`;
 
-    getUserContacts(userId) {
-        return new Promise((resolve, reject) => {
-            const query = `SELECT UC.id, C.name, UC.value, C.icon, UC.user_id
+      db.query(query, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result[0][0].media);
+        }
+      });
+    });
+  },
+
+  getUserContacts(userId) {
+    return new Promise((resolve, reject) => {
+      const query = `SELECT UC.id, C.name, UC.value, C.icon, UC.user_id
             FROM user_contact UC
             INNER JOIN contact C ON C.id = UC.contact_id
             WHERE UC.user_id = ${userId}`;
 
-            db.query(query, (error, result) => {
-                if (error) {
-                    reject(error)
-                } else {
-                    resolve(result);
-                }
-            });
-        });
-    }
+      db.query(query, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  },
 };
